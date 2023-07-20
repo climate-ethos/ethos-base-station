85b769e78127ce1c50374eeb569028b5542436ea
Connor Forbes <connorforbes25@gmail.com>
2023-07-20 15:51:05 +1000

    demo: Fix demo alert

--- javascript_ui/src/components/BaseCurrentTime.vue
+++ javascript_ui/src/components/BaseCurrentTime.vue
@@ -1,21 +1,22 @@
 <template>
-  <q-chip
-    @click="showDemoAlert"
-    color="transparent"
-    text-color="primary"
-    class="text-bold"
-  >
-    {{ currentTime }}
-  </q-chip>
+  <div @click="showDemoAlert">
+    <q-chip color="transparent" text-color="primary" class="text-bold">
+      {{ currentTime }}
+    </q-chip>
+  </div>
 </template>
 
 <script lang="ts">
 import { ref, onMounted } from 'vue';
 import { useDataSensorStore } from 'src/stores/dataSensor';
+import { useDataPreferencesStore } from 'src/stores/dataPreferences';
+import { playAudio } from 'src/helpers/audioAlertDispatcher';
+import { RiskLevel } from './models';
 
 export default {
   setup() {
     const dataSensorStore = useDataSensorStore();
+    const dataPreferencesStore = useDataPreferencesStore();
     const currentTime = ref('');
 
     const getCurrentTime = () => {
@@ -29,6 +30,11 @@ export default {
     const showDemoAlert = () => {
       console.log('Showing alert');
       dataSensorStore.alertSensor = dataSensorStore.allSensorData[1];
+      playAudio(
+        dataPreferencesStore.audioType,
+        RiskLevel.MEDIUM,
+        dataSensorStore.alertSensor
+      );
     };
 
     onMounted(() => {
