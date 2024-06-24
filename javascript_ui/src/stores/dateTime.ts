df5188aa579c3700b9e31839715c27ba4cfee06b
Connor Forbes <connorforbes25@gmail.com>
2024-06-24 15:51:34 +1000

    feat: Fix time to 12pm

--- javascript_ui/src/stores/dateTime.ts
+++ javascript_ui/src/stores/dateTime.ts
@@ -1,16 +1,18 @@
 import { defineStore } from 'pinia';
 
+const dateString = 'January 15, 2024 12:13:00';
+
 export const useDateTimeStore = defineStore({
   id: 'dateTime',
 
   state: () => ({
-    currentDate: new Date(),
+    currentDate: new Date(dateString),
   }),
 
   actions: {
     updateCurrentDate() {
       console.log('updating date...');
-      this.currentDate = new Date();
+      this.currentDate = new Date(dateString);
     },
 
     startInterval() {
